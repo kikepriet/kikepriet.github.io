8c6eefb068206efeede49339c84289d3c61eae5f
Jesús Enrique Prieto Treviño <jept@tec.mx>
2023-10-06 13:06:51 -0600

    Probando un formulario

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include 'Templates/header.php';?>
+
+
+<form>
+    <?include 'Preguntas/pregunta1.php';?>
+    
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+
+
+  <?php include 'Templates/footer.php';?>
